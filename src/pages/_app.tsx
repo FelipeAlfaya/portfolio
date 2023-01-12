56809823fcb28e7062a77443dd021ad240611f5f
FelipeAlfaya <felipealfaya90@gmail.com>
2023-01-12 18:42:56 -0300

    deploy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,14 @@ import GlobalStyles from '../styles/global'
 import { ThemeProvider } from 'styled-components'
 import theme from '../styles/theme'
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Felipe Alfaya</title>
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyles />
         <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -47,9 +47,9 @@ export default class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap'
             rel='stylesheet'
           />
+          <link rel='icon' href='code.png' type='image/x-icon' />
           <meta charSet='utf-8' />
           <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-          <title>Felipe Alfaya</title>
         </Head>
         <body>
           <Main />
